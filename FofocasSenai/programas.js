67d9e761531181b7c97289c7d0c32aa3764e9e0e
Bruno Henrique <146200623+BrunoHFJ@users.noreply.github.com>
2024-07-04 19:49:42 -0300

    Add files via upload

---
+++ FofocasSenai/programas.js
@@ -0,0 +1,72 @@
+"use strict"
+// window.alert ("Olá Mundo!")
+
+/*var datanascimento, idade
+datanascimento = prompt("Em qual ano você nasceu?")
+idade = (2024 -  datanascimento)
+window.alert ("Sua idade é " + idade);*/
+
+/*var nota1, nota2, nota3, media
+nota1 = parseFloat (prompt("Qual nota do primeiro trimestre?"))
+nota2 = parseFloat (prompt("Qual nota do segundo trimestre?"))
+nota3 = parseFloat (prompt("Qual nota do terceiro trimestre?"))
+media = (nota1+nota2+nota3)/3
+if(media>=60){
+    window.alert ("Sua media foi "+ media + " e você esta aprovado!") 
+             }else if(media>=40 && media<60){
+                  window.alert ("Sua media foi "+ media + " e você esta de recuperação!")
+                                            }else{
+                                              window.alert ("Sua media foi "+ media + " e você esta reprovado!")
+                                                 }*/
+
+//QUESTÃO 1                                             
+/*var nome, dia, mes
+nome = (prompt("Qual seu nome?"))
+dia = (prompt("Fale um dia?"))
+mes = (prompt("Fale um mês(em número)?"))
+if(nome=="Celso Portiolli" && dia==11 && mes==9){
+  window.alert("O Celso Portiolli não tem nada a ver com o 11 de Setembro")
+}else if(nome=="Bruno" && dia==4 && mes==7){
+  window.alert("Faça a prova com atenção")
+}else{
+  window.alert("Tururu(música do naruto)")
+}*/
+
+//QUESTÃO 2
+/*var nota1, nota2, nota3, media
+nota1 = parseFloat (prompt("Qual nota do primeiro trimestre?"))
+nota2 = parseFloat (prompt("Qual nota do segundo trimestre?"))
+nota3 = parseFloat (prompt("Qual nota do terceiro trimestre?"))
+media = (nota1+nota2+nota3)/3
+if(media>=60){
+    window.alert ("Sua media foi "+ media + " e você esta aprovado!") 
+             }else{
+                 window.alert ("Sua media foi "+ media + " e você esta reprovado!")
+             }*/
+
+//QUESTÃO 3
+/*var gado, pesoTotal, peso, mediaPeso, numeroGado
+gado=0
+pesoTotal=0
+numeroGado=1
+
+while(peso!=0){
+  peso = parseFloat (prompt("Qual peso do gado "+numeroGado+"?"))
+  pesoTotal = pesoTotal+peso
+  gado = gado+1
+  numeroGado = numeroGado+1
+}
+gado = gado-1
+mediaPeso = pesoTotal/gado
+
+window.alert("O peso médio é "+mediaPeso)*/
+
+
+//QUESTÃO 4
+/*var num1, num2, resultado
+window.alert("Vamos calcular a potencia de dois números!")
+num1 = parseFloat (prompt("Digite o primeiro número:"))
+num2 = parseFloat (prompt("Digite o segundo número:"))
+resultado = num1 ** num2
+window.alert("O resultado é: "+resultado)*/
+             
